373831189dd1c8278f3a96a129ee58778f10971a
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-12-13 23:36:34 +0100

    🚧 add Inject Block function 🚧

--- InjectPass/LLVMPassInjectBlock/main.cpp
+++ InjectPass/LLVMPassInjectBlock/main.cpp
@@ -1,19 +1,30 @@
 #include "llvm/IR/PassManager.h"
-#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-#include <map>
+#include <vector>
 using namespace llvm;
 
 namespace {
   struct InjectBlockPass : public PassInfoMixin<InjectBlockPass> {
-    Function* createFunc(Module &M) {
-      auto &Ctx = M.getContext();
+    void injectBlock(Module &M,Function *F) {
+      auto &Ctx = F->getContext();
+      BasicBlock &EntryBlock = F->getEntryBlock();
+      BasicBlock *PrintBlock = BasicBlock::Create(Ctx,"PrintBlock",EntryBlock.getParent(),&EntryBlock);
+      Function *printfFunction = M.getFunction("printf");
+
+      IRBuilder<> Builder(PrintBlock);
+      std::vector<Value *> printArgs;
+      Value *FormatStr = Builder.CreateGlobalStringPtr("Block Injected\n");
+      printArgs.push_back(FormatStr);
+      Builder.CreateCall(printfFunction,printArgs);
+      Builder.CreateBr(&EntryBlock);
     }
     PreservedAnalyses run(Module &M,ModuleAnalysisManager &MAM) {
       Function *MainFunction = M.getFunction("main");
-      createBlock(MainFunction,);
+      injectBlock(M,MainFunction);
       return PreservedAnalyses::all();
     }
   };
